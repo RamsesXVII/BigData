89d9a30eecb7250955b6c9cbe31d267bf390df34
hyruma <valrim992@gmail.com>
2017-05-16 17:29:15 +0200

    Dio aiutaci

--- homework1Spark/task2.py
+++ homework1Spark/task2.py
@@ -8,8 +8,7 @@ sc = spark.sparkContext
 start = int(round(time.time() * 1000))
 
 records = sc.textFile(sys.argv[1])
-recP = sc.parallelize(records.collect())
-rows = recP.map(lambda line: line.split("\t"))
+rows = records.map(lambda line: line.split("\t"))
 userToFavouriteProducts=rows.map(lambda x: [x[1],x[2],x[6]]).map(lambda n: (str(n[1]),str(str(n[2])+"-"+str(n[0])))).groupByKey().map(lambda x: {x[0]: sorted(list(x[1]))[-10:]}).collect()
 
 end = int(round(time.time() * 1000))
@@ -17,7 +16,8 @@ end = int(round(time.time() * 1000))
 out_file= open(sys.argv[2], "w+")
 
 for row in sorted(userToFavouriteProducts):
-    out_file.write(row)
+    out_file.write(str(row))
+    out_file.write("\n")
 
 out_file.close()
 print (end - start)
--- homework1Spark/task3.py
+++ homework1Spark/task3.py
@@ -8,13 +8,12 @@ sc = spark.sparkContext
 start = int(round(time.time() * 1000))
 
 records = sc.textFile(sys.argv[1])
-recP= sc.parallelize(records.collect())
-rows = recP.map(lambda line: line.split("\t"))
-a=rows.map(lambda x: (x[1],[x[2],x[6]])).filter(lambda (k,x): int(x[1])>=4)
+rec= sc.parallelize(records.collect())
 
-a=a.join(a).filter(lambda (x,(a,b)): a[0]<b[0]).map(lambda (x,(b,a)): ((a[0],b[0]),x)).groupByKey().filter(lambda (x,y): len(set(y))>2$
+rows = rec.map(lambda line: line.split("\t"))
+a=rows.map(lambda x: (x[1],[x[2],x[6]])).filter(lambda (k,v): int(v[1])>=4)
 
-end = int(round(time.time() * 1000))
+a=a.join(a).filter(lambda (x,(a,b)): a[0]<b[0]).map(lambda (x,(b,a)): ((a[0],b[0]),x)).groupByKey().filter(lambda (x,y): len(set(y))>2).sortBy(lambda a: a[0]).collect()
 
 out_file= open(sys.argv[2], "w+")
 
@@ -28,4 +27,6 @@ for c in a:
 	out_fle.write("\n")
 
 out_file.close()
+
+end = int(round(time.time() * 1000))
 print (end - start)
