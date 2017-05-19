783e4b0d43e3dbe5abfd257d91fed8e8f73fe317
Mattia Iodice <mattia.iodice3@gmail.com>
2017-05-19 13:57:11 +0200

    Rimuovo parallelize

--- homework1Spark/task3.py
+++ homework1Spark/task3.py
@@ -7,8 +7,8 @@ sc = spark.sparkContext
 
 start = int(round(time.time() * 1000))
 
-records = sc.textFile(sys.argv[1])
-rec= sc.parallelize(records.collect())
+rec = sc.textFile(sys.argv[1])
+#rec= sc.parallelize(records.collect())
 
 rows = rec.map(lambda line: line.split("\t")).filter(lambda x: int(x[6])>=4)
 a=rows.map(lambda x: (x[1],[x[2], x[6]]))
@@ -16,4 +16,7 @@ a=rows.map(lambda x: (x[1],[x[2], x[6]]))
 a=a.join(a).filter(lambda (x,(a,b)): a[0]<b[0]).map(lambda (x,(b,a)): ((a[0],b[0]),x)).groupByKey().filter(lambda (x,y): len(set(y))>2).sortBy(lambda a: a[0]).saveAsTextFile(sys.argv[2])
 
 end = int(round(time.time() * 1000))
+
+print("****************************")
 print (end - start)
+print("****************************")
