3ea390181b95a57c1668e425089bc13ba976b21d
hyruma <valrim992@gmail.com>
2017-05-19 14:30:39 +0200

    task1 fix

--- homework1Spark/task1.py
+++ homework1Spark/task1.py
@@ -12,7 +12,7 @@ records = sc.textFile(sys.argv[1])
 seqOp = lambda x, y : (float(x[0]) + float(y), float(x[1]) + 1)
 combOp = lambda x, y : (float(x[0]) + float(y[0]), float(x[1]) + float(y[1]))
 rows = records.map(lambda line: line.split("\t"))
-result = rowsmap(lambda x: ((str(datetime.datetime.fromtimestamp(int(x[7])).strftime('%Y-%m')) + " " +x[1]),x[6])).aggregateByKey((0,0), seqOp, combOp).mapValues(lambda v: v[0]/v[1]).map(lambda (k,v): (k.split(" ")[0], str(v) + " " + k.split(" ")[1])).groupByKey().sortByKey().map(lambda x: {x[0]: sorted(list(x[1]))[-5:]}).collect()
+result = rows.map(lambda x: ((str(datetime.datetime.fromtimestamp(int(x[7])).strftime('%Y-%m')) + " " +x[1]),x[6])).aggregateByKey((0,0), seqOp, combOp).mapValues(lambda v: v[0]/v[1]).map(lambda (k,v): (k.split(" ")[0], str(v) + " " + k.split(" ")[1])).groupByKey().sortByKey().map(lambda x: {x[0]: sorted(list(x[1]))[-5:]}).collect()
 
 out_file= open(sys.argv[2], "w+")
 
