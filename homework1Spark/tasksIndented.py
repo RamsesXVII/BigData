145b135b8f58d2c628cb505b2ca7a0eb50616b6c
hyruma <valrim992@gmail.com>
2017-05-22 15:26:35 +0200

    finale task2

--- homework1Spark/tasksIndented.py
+++ homework1Spark/tasksIndented.py
@@ -36,8 +36,7 @@ sc = spark.sparkContext
 records = sc.textFile(sys.argv[1])
 rows = records.map(lambda line: line.split("\t"))
 
-userToFavouriteProducts=rows.map(lambda x: [x[1],x[2],x[6]])
-                            .map(lambda n: (str(n[1]),str(str(n[2])+"-"+str(n[0]))))
+userToFavouriteProducts=rows.map(lambda x: (x[2],x[6] +"-"+ x[1]))
                             .groupByKey()
                             .map(lambda x: {x[0]: sorted(list(x[1]))[-10:]})
                             .collect()
